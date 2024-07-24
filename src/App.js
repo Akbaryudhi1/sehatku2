31ca3ef4497ce13ba09c79394a02d3c7f713853a
Akbaryudhi1 <121375769+Akbaryudhi1@users.noreply.github.com>
2024-07-24 14:33:30 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Router from "./routes";
+import Layout from "./layout";
+function App() {
+  return (
+    <>
+      <Layout>
+      <Router />
+      </Layout>
+    </>
+      )
+}
+
+export default App;
